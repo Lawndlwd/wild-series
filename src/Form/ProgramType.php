b1218c3ebabd9117b125713156928ed1e86a1acd
Levend <lwdlwd95@gmail.com>
2020-06-08 10:01:17 +0200

    gf

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Category;
 use App\Entity\Program;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -18,6 +19,13 @@ class ProgramType extends AbstractType
             ->add('summary')
             ->add('poster')
             ->add('category', EntityType::class, ['class'=>Category::class, 'choice_label'=>'name'])
+            ->add('actors' , EntityType::class , [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple'=>true,
+                'expanded'=> true,
+                'by_reference'=>false,
+            ] )
         ;
     }
 
